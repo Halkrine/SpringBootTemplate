5b661ac693f1ffae73da3eacf9d26e5db5ccd6fd
Halkrine <halkrine@gmail.com>
2018-10-30 21:05:41 +0900

    repository add

--- src/main/java/com/template/controller/ExcelTestRestController.java
+++ src/main/java/com/template/controller/ExcelTestRestController.java
@@ -1,5 +1,14 @@
 package com.template.controller;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.streaming.SXSSFCell;
+import org.apache.poi.xssf.streaming.SXSSFRow;
+import org.apache.poi.xssf.streaming.SXSSFSheet;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +22,27 @@ public class ExcelTestRestController {
 	
 	
 	@RequestMapping(value="/excelDownLoad")
-	public void excelDownLoad() {
+	public void excelDownLoad() throws IOException {
+		SXSSFWorkbook workbook = new SXSSFWorkbook();
+		
+		SXSSFSheet sheet = (SXSSFSheet) workbook.createSheet("Sheet");
+		
+		SXSSFRow row = sheet.createRow(0);
+		
+		SXSSFCell cell = row.createCell(0);
+		cell.setCellValue("test");
+		
+		FileOutputStream fos = new FileOutputStream("/Users/halkrine/temp.xlsx");
+		
+		workbook.write(fos);
+	}
+	
+	@RequestMapping(value="/readExcel")
+	public void readExcel() throws FileNotFoundException {
+		FileInputStream fis = new FileInputStream("/Users/halkrine/temp.xlsx");
+		
+		SXSSFWorkbook workbook = new SXSSFWorkbook();
+		
 		
 	}
 }
